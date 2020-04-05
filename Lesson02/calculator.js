67074dc5f055fdf426d701967105be5538eab368
paulfleurdelys <paul.fleurdelys@gmail.com>
2020-04-04 19:53:39 -0700

    Adding calculator.js

---
+++ Lesson02/calculator.js
@@ -0,0 +1,27 @@
+let x = 10;
+let y = 5;
+
+add(x, y);
+subtract(x, y);
+multiply(x, y);
+divide(x, y);
+
+function add(x, y) {
+    console.log(x + y);
+    //15
+}
+
+function subtract(x, y) {
+    console.log(x-y);
+    //5
+}
+
+function multiply(x, y) {
+    console.log(x * y);
+    //50
+}
+
+function divide(x, y) {
+    console.log(x / y);
+    //2
+}
